735a01faf3fea552873d788e44d0781d83ae0050
KUba935 <kubsonix198@gmail.com>
2025-09-16 16:28:38 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,27 @@
+
+a = 10
+b = "XD"
+c = True
+
+if a == 10:
+    print(b)
+elif a == 132:
+    print("xd")
+else:
+    print(c)
+    
+for i in range(1, 101):
+     if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print ("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+
+
+
+
+
